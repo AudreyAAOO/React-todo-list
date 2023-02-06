c53fede62784f5ae75261dd954f1d9acfff1d9c3
AudreyAAOO <audrey.s.carpentier@gmail.com>
2023-02-06 16:03:12 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import {
   faListCheck,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-library.add(faSquareCheck,  faSquare, faListCheck);
+library.add(faSquareCheck, faSquare, faListCheck);
 function App() {
 
   //! STATE (état, données) *・゜゜・*:.。..。.:*・*:゜・*:.。. .。.:*・゜゜・**・゜゜・*:.。..。.:*・*:゜・*:.。. .。.:*・゜゜・**・゜゜・*:.。..。.:*
-  // eslint-disable-next-line
+
   const [tasks, setTasks] = useState([]);
   const [writeTask, setWriteTask] = useState("");
 
@@ -26,6 +26,9 @@ function App() {
     const copyTasks = [...tasks];  //? Spread operators pour pourvoir manipuler un clone du tableau (fonctionne aussi avec slice() )
     // 2. manipuler mon state
     const tasksCopyUpdated = copyTasks.filter(task => task.id !== id) //? Filtrer le tab pour ne garder que les éléments qui n'ont pas été cliqué, donc ceux dont l'id est différent de celui reçu en paramètre
+
+    //ou avec splice() :  const tasksCopyUpdated = copyTasks.splice(index,1) supprimer 1 élément à partir du début de coupe donné
+
     // 3. modifier mon state avec le setter
     setTasks(tasksCopyUpdated);
   }
@@ -37,15 +40,16 @@ function App() {
     // 1. copie du state
     const tasksCopy = [...tasks];
     // 2. manipuler mon state
-    const id = new Date().getTime() //? pour rendre l'id unique à l'instant T de sa création
-    const nom = writeTask
-    tasksCopy.push({ id: id, nom: nom }); // pusher un nouvel objet qui a un id et un nom 
+    const id = new Date().getTime(); //? pour rendre l'id unique à l'instant T de sa création
+    tasksCopy.push({
+      id: id,
+      nom: writeTask,
+      isDone: false,
+    }); // pusher un nouvel objet qui a un id et un nom 
     // 3. modifier mon state avec le setter
     setTasks(tasksCopy);
     setWriteTask(""); //? vider l'input après la saisie
   }
-  // 
-  // setWriteTask(copyWriteTask);
 
 
   const handleAddTask = (event) => { // la fonction reçoit l'évent lors du changement de l'input (= quand on écrit dedans)
@@ -54,42 +58,63 @@ function App() {
     setWriteTask(valueAfterChange);  // que je donne à mon state grâce au setter et ça update l'affichage
   };
 
+
+  const handleCheck = (index) => {
+    // 1. copie du state
+    const copyTasks = [...tasks];
+    // 2. manipuler mon state
+    copyTasks[index].isDone = !copyTasks[index].isDone;
+    // 3. modifier mon state avec le setter
+    setTasks(copyTasks);
+  };
+
+
   //! RENDER (affichage) *・゜゜・*:.。..。.:*・*:゜・*:.。. .。.:*・゜゜・**・゜゜・*:.。..。.:*・*:゜・*:.。. .。.:*・゜゜・**・゜゜・*:.。..。.:*
   return (
     <>
       <div className="App">
-        <header><h1>To Do List  <FontAwesomeIcon icon="list-check" className="icon"/> </h1></header>
+        <header><h1>To Do List
+        </h1>
+        </header>
+        <body>
 
-        <form action="submit" onSubmit={handleSubmit}>
+
+          <form action="submit" onSubmit={handleSubmit}>
 
 
-          {/*//! méthode MAP() {/* boucler sur le tableau Tasks et retourner un autre...  */}
-          {/*  ...tableau transformé par une fonction et afficher 1 à 1 des éléments <li> */}
-          <ul>
-            {tasks.map((task) => {
-              {/* task fait référence à chacun des éléments du tableau taskS */ }
-              return (<>
-              
-                <Tasks listTaches={task} tacheASupprimer={handleDelete} />
-                {/* <li key={task.id}> <FontAwesomeIcon icon="list-check" /> 
-                {task.nom} <button onClick={() => handleDelete(task.id)}>x</button>
-              </li> */}
+            {/*//! méthode MAP() {/* boucler sur le tableau Tasks et retourner un autre...  */}
+            {/*  ...tableau transformé par une fonction et afficher 1 à 1 des éléments <li> */}
+            <ul>
+              {tasks.map((task, index) => {
+                {/* task fait référence à chacun des éléments du tableau taskS */ }
+                return (<>
 
-              </>)
-            })}
+                  <div key={index}>
+                    <li>
+                      <input type="checkbox" key={task.id} checked={task.isDone} onChange={() => {
+                        handleCheck(index);
+                      }} /></li>
+                    <li>
+                      <span className={task.isDone ? 'isDone' : ""}>{task.nom} </span></li>
 
-          </ul>
+                    <li>
+                      <Tasks listTaches={task} tacheASupprimer={handleDelete} />
+                    </li></div>
+                </>)
+              })}
 
-          <input
-            // Le contenu de mon input est ce qu'il y a dans ce stateonClick={AddTask}
-            value={writeTask}
-            type="text"
-            placeholder="ajouter un truc à faire"
-            onChange={handleAddTask} // donne la possibilité à l'input de changer
-          />
-          <button >+ Ajouter une tâche</button>
+            </ul>
 
-        </form>
+            <input
+              // Le contenu de mon input est ce qu'il y a dans ce stateonClick={AddTask}
+              value={writeTask}
+              type="text"
+              placeholder="ajouter un truc à faire"
+              onChange={handleAddTask} // donne la possibilité à l'input de changer
+            />
+            <button >+ Ajouter une tâche</button>
+
+          </form></body>
       </div >
     </>
   )
--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -13,8 +13,8 @@ export default function Tasks(props) {
 
 return  (
         <li key={props.listTaches.id}> 
-        <FontAwesomeIcon icon="square" className="icon"/> 
-        {props.listTaches.nom} 
+     
+     
         
         <button className='btnDel' onClick={() => props.tacheASupprimer(props.listTaches.id)}>x</button>
         
